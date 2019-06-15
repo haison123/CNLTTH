47d7d65f1d963bb637b7b0d0f86e876f7efbf31b
TungPham9801 <50493321+TungPham9801@users.noreply.github.com>
2019-06-15 16:37:21 +0700

    t
    t

--- QuanLiCaPhe/frmReportHD.Designer.cs
+++ QuanLiCaPhe/frmReportHD.Designer.cs
@@ -28,32 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReportHD));
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.label1 = new System.Windows.Forms.Label();
             this.txtMaOder = new System.Windows.Forms.TextBox();
             this.btnXem = new System.Windows.Forms.Button();
-            this.crystalReportViewer_HD = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.btnThoat = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtMaBan = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ReportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.QuanLyCaPheDataSet = new QuanLiCaPhe.QuanLyCaPheDataSet();
+            this.tblCTHoaDonBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tblCTHoaDonTableAdapter = new QuanLiCaPhe.QuanLyCaPheDataSetTableAdapters.tblCTHoaDonTableAdapter();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.QuanLyCaPheDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblCTHoaDonBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(670, 8);
+            this.label1.Location = new System.Drawing.Point(244, 115);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 19);
+            this.label1.Size = new System.Drawing.Size(95, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Số Oder:";
             // 
             // txtMaOder
             // 
             this.txtMaOder.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMaOder.Location = new System.Drawing.Point(753, 4);
+            this.txtMaOder.Location = new System.Drawing.Point(406, 115);
+            this.txtMaOder.Margin = new System.Windows.Forms.Padding(4);
             this.txtMaOder.Name = "txtMaOder";
-            this.txtMaOder.Size = new System.Drawing.Size(157, 26);
+            this.txtMaOder.Size = new System.Drawing.Size(208, 30);
             this.txtMaOder.TabIndex = 1;
             // 
             // btnXem
@@ -61,34 +75,24 @@
             this.btnXem.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnXem.Image = ((System.Drawing.Image)(resources.GetObject("btnXem.Image")));
             this.btnXem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnXem.Location = new System.Drawing.Point(489, 49);
+            this.btnXem.Location = new System.Drawing.Point(836, 31);
+            this.btnXem.Margin = new System.Windows.Forms.Padding(4);
             this.btnXem.Name = "btnXem";
-            this.btnXem.Size = new System.Drawing.Size(125, 34);
+            this.btnXem.Size = new System.Drawing.Size(179, 42);
             this.btnXem.TabIndex = 2;
             this.btnXem.Text = "XEM";
             this.btnXem.UseVisualStyleBackColor = true;
             this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
-            // 
-            // crystalReportViewer_HD
-            // 
-            this.crystalReportViewer_HD.ActiveViewIndex = -1;
-            this.crystalReportViewer_HD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.crystalReportViewer_HD.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crystalReportViewer_HD.Location = new System.Drawing.Point(2, 97);
-            this.crystalReportViewer_HD.Name = "crystalReportViewer_HD";
-            this.crystalReportViewer_HD.SelectionFormula = "";
-            this.crystalReportViewer_HD.Size = new System.Drawing.Size(1358, 398);
-            this.crystalReportViewer_HD.TabIndex = 3;
-            this.crystalReportViewer_HD.ViewTimeSelectionFormula = "";
             // 
             // btnThoat
             // 
             this.btnThoat.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
             this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnThoat.Location = new System.Drawing.Point(727, 49);
+            this.btnThoat.Location = new System.Drawing.Point(836, 106);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(134, 34);
+            this.btnThoat.Size = new System.Drawing.Size(179, 42);
             this.btnThoat.TabIndex = 4;
             this.btnThoat.Text = "THOÁT";
             this.btnThoat.UseVisualStyleBackColor = true;
@@ -98,37 +102,101 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(413, 8);
+            this.label2.Location = new System.Drawing.Point(244, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 19);
+            this.label2.Size = new System.Drawing.Size(86, 24);
             this.label2.TabIndex = 5;
             this.label2.Text = "Mã bàn:";
             // 
             // txtMaBan
             // 
             this.txtMaBan.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMaBan.Location = new System.Drawing.Point(489, 6);
+            this.txtMaBan.Location = new System.Drawing.Point(406, 31);
+            this.txtMaBan.Margin = new System.Windows.Forms.Padding(4);
             this.txtMaBan.Name = "txtMaBan";
-            this.txtMaBan.Size = new System.Drawing.Size(157, 26);
+            this.txtMaBan.Size = new System.Drawing.Size(208, 30);
             this.txtMaBan.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnThoat);
+            this.groupBox1.Controls.Add(this.txtMaBan);
+            this.groupBox1.Controls.Add(this.btnXem);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMaOder);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1438, 188);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin";
+            // 
+            // ReportViewer
+            // 
+            this.ReportViewer.Location = new System.Drawing.Point(0, 0);
+            this.ReportViewer.Name = "ReportViewer";
+            this.ReportViewer.ServerReport.BearerToken = null;
+            this.ReportViewer.Size = new System.Drawing.Size(396, 246);
+            this.ReportViewer.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.reportViewer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 188);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1438, 536);
+            this.panel1.TabIndex = 8;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.tblCTHoaDonBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "QuanLiCaPhe.HoaDonReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(1438, 536);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // QuanLyCaPheDataSet
+            // 
+            this.QuanLyCaPheDataSet.DataSetName = "QuanLyCaPheDataSet";
+            this.QuanLyCaPheDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblCTHoaDonBindingSource
+            // 
+            this.tblCTHoaDonBindingSource.DataMember = "tblCTHoaDon";
+            this.tblCTHoaDonBindingSource.DataSource = this.QuanLyCaPheDataSet;
+            // 
+            // tblCTHoaDonTableAdapter
+            // 
+            this.tblCTHoaDonTableAdapter.ClearBeforeFill = true;
             // 
             // frmReportHD
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1362, 689);
-            this.Controls.Add(this.txtMaBan);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.btnThoat);
-            this.Controls.Add(this.crystalReportViewer_HD);
-            this.Controls.Add(this.btnXem);
-            this.Controls.Add(this.txtMaOder);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1438, 724);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmReportHD";
             this.Text = "frmReportHD";
+            this.Load += new System.EventHandler(this.frmReportHD_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.QuanLyCaPheDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblCTHoaDonBindingSource)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -137,10 +205,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtMaOder;
         private System.Windows.Forms.Button btnXem;
-        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer_HD;
         private System.Windows.Forms.Button btnThoat;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtMaBan;
-
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Microsoft.Reporting.WinForms.ReportViewer ReportViewer;
+        private System.Windows.Forms.Panel panel1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource tblCTHoaDonBindingSource;
+        private QuanLyCaPheDataSet QuanLyCaPheDataSet;
+        private QuanLyCaPheDataSetTableAdapters.tblCTHoaDonTableAdapter tblCTHoaDonTableAdapter;
     }
 }
--- QuanLiCaPhe/frmReportHD.cs
+++ QuanLiCaPhe/frmReportHD.cs
@@ -9,14 +9,16 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 using BUS_QLCP;
 using DTO_QLCP;
-using QuanLiCaPhe.ReportService;
+using QuanLiCaPhe.CTHoadonService;
+using QuanLiCaPhe.HoaDonService;
+using Microsoft.Reporting.WinForms;
 
 namespace QuanLiCaPhe
 {
     public partial class frmReportHD : Form
     {
-        ReportServicesClient rp = new ReportServicesClient();
-        CTHoaDonBO cthdBO = new CTHoaDonBO();
+        HoaDonServiceClient hoadon = new HoaDonServiceClient();
+        CTHoaDonServiceClient cthoadon = new CTHoaDonServiceClient();
         public frmReportHD(string text1,string text2)
         {
             InitializeComponent();
@@ -26,15 +28,41 @@ namespace QuanLiCaPhe
 
         private void btnXem_Click(object sender, EventArgs e)
         {
-            int maod = int.Parse(txtMaOder.Text); ;
-            string mahd = cthdBO.layMaHD(maod);
-            int i = 0;
-            Int32.TryParse(mahd, out i);
+            int maod = int.Parse(txtMaOder.Text);
+            string mahd = cthoadon.layMaHD(maod);
             
-            Report_HD objRpt = new Report_HD();
-            objRpt.SetDataSource(rp.report_HD(i).Tables[1]);
-            crystalReportViewer_HD.ReportSource = objRpt;
-            crystalReportViewer_HD.Refresh();        
+            
+            DataTable dt = new DataTable();
+            dt = cthoadon.LayDSCTHD(Convert.ToInt32(mahd));
+            
+            reportViewer1.ProcessingMode = Microsoft.Reporting.WinForms.ProcessingMode.Local;
+            reportViewer1.LocalReport.ReportPath = "HoaDonReport.rdlc";
+                ReportDataSource source = new ReportDataSource();
+                source.Name = "DataSet1";
+            source.Value = dt;
+                //
+                reportViewer1.LocalReport.DataSources.Clear();
+                reportViewer1.LocalReport.DataSources.Add(source);
+
+                reportViewer1.RefreshReport();
+            
+
+
+            //int i = 0;
+            //Int32.TryParse(mahd, out i);
+            //SqlConnection connect;
+            //connect = new SqlConnection(@"server=.\SQLEXPRESS; Database=QuanLiCaPhe;integrated security=true");
+            //connect.Open();
+            //SqlDataAdapter da = new SqlDataAdapter();
+            //string query = "SELECT MaHD,MaMon,TenMon,SoLuong,DonGia,ThanhTien FROM tblCTHoaDon where MaHD='" + i + "'";
+            //SqlDataAdapter dscm = new SqlDataAdapter(query, connect);
+            //DSHoaDon ds = new DSHoaDon();
+            //dscm.Fill(ds, "tblCTHoaDon");
+            //connect.Close();
+            //Report_HD objRpt = new Report_HD();
+            //objRpt.SetDataSource(ds.Tables[1]);
+            //crystalReportViewer_HD.ReportSource = objRpt;
+            //crystalReportViewer_HD.Refresh();        
         }
 
         private void btnThoat_Click(object sender, EventArgs e)
@@ -42,6 +70,14 @@ namespace QuanLiCaPhe
            frmSoDoChinh SoDoChinh=new frmSoDoChinh();         
            SoDoChinh.Show();
            this.Close();
+        }
+
+        private void frmReportHD_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'QuanLyCaPheDataSet.tblCTHoaDon' table. You can move, or remove it, as needed.
+            //this.tblCTHoaDonTableAdapter.Fill(this.QuanLyCaPheDataSet.tblCTHoaDon);
+
+           // this.reportViewer1.RefreshReport();
         }
     }
 }
